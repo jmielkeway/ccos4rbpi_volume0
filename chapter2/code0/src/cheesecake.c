30d345fc37e2e2ac4eca496e8a437390cf6a76d4
jmielkeway <jmielkeway@gmail.com>
2021-02-06 15:29:54 +0100

    :computer: Add Code for Chapter Two Code Zero (#36)
    Problem:
    ---
    - Must initialize the processor
    
    Solution:
    
    ---
    - Include board_specifc_secure_boot function
    - Drop down to EL1 from EL3
    
    Issue: #28

---
+++ chapter2/code0/src/cheesecake.c
@@ -0,0 +1,39 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cake/log.h"
+#include "arch/timing.h"
+
+extern void log_init();
+void init();
+
+void cheesecake_main(void)
+{
+    unsigned long count = 1;
+    char *version = "0.2.4";
+    init();
+    log("Hello, Cheesecake!\r\n");
+    while (1) {
+        log("Version: %s\r\n", version);
+        log("Message count: %x\r\n", count++);
+        log("\r\n");
+        DELAY(20000000);
+    }
+}
+
+void init()
+{
+    log_init();
+    log("LOG MODULE INITIALIZED\r\n");
+}
