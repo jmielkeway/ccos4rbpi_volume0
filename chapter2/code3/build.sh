8b3b2e64bb3137527994d8d841b9f0e8f9faa0bd
jmielkeway <jmielkeway@gmail.com>
2021-02-08 19:12:43 +0000

    :computer: Add Timer Initialization in Chapter Two Code Three
    Problem:
    ---
    - First IRQ to work with is the timer interrupt
    - It needs initialization
    
    Solution:
    ---
    - Use system timer 3 with SPID 99
    - Add memory barriers around device reads/writes
    
    Issue: #41

---
+++ chapter2/code3/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+if [[ $1 == "clean" ]]; then
+    docker run --rm -v $(pwd):/ccos4rbpi -w /ccos4rbpi -u $(id -u ) ccos4rbpi make clean
+else
+    docker run --rm -v $(pwd):/ccos4rbpi -w /ccos4rbpi -u $(id -u ) ccos4rbpi make exec ARCH=arm64
+    docker run --rm -v $(pwd):/ccos4rbpi -w /ccos4rbpi -u $(id -u ) ccos4rbpi make ARCH=arm64 BOARD=raspberry-pi-4
+fi
