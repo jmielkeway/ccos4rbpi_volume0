c3955d064cd213608ad8fbd6952e3d9d2dd4f8da
jmielkeway <jmielkeway@gmail.com>
2021-02-21 00:19:42 +0000

    :computer: Initialize Free Lists in Chapter Six Code Zero
    Problem:
    ---
    - Need to manage memory
    - First must track all pages
    - Must also setup freelists
    
    Solution:
    ---
    - Create list struct and functions
    - Create system physical page directory to track all pages
    - Initialize page lists
    - Free baby boot and overwrite sections as well
    
    Issue: #80

---
+++ chapter6/code0/src/allocate.c
@@ -0,0 +1,46 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cake/allocate.h"
+#include "cake/list.h"
+#include "cake/log.h"
+
+extern void arch_populate_allocate_structures(struct page **system_phys_page_dir, 
+    struct list *freelists);
+
+static struct list freelists[MAX_ORDER + 1];
+static struct page *system_phys_page_dir;
+
+void allocate_init()
+{
+    for(unsigned int i = 0; i <= MAX_ORDER; i++) {
+        struct list *freelist = &(freelists[i]);
+        freelist->next = freelist;
+        freelist->prev = freelist;
+    }
+    arch_populate_allocate_structures(&system_phys_page_dir, freelists);
+    log("Page dir location: %x\r\n", (unsigned long) system_phys_page_dir);
+    for(unsigned long i = 0; i <= MAX_ORDER ; i++) {
+        struct list *freelist = &(freelists[i]);
+        struct page *p;
+        log("Printing freelist for order: %x\r\n", i);
+        LIST_FOR_EACH_ENTRY(p, freelist, pagelist) {
+            log("{Page Details: index: %x, allocated: %x, valid: %x}\r\n", 
+                p->pfn, p->allocated, p->valid);
+            log("{List Details: next: %x, prev: %x}\r\n", 
+                p->pagelist.next, p->pagelist.prev);
+        }
+    }
+}
+
