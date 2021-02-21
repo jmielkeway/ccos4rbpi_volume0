955bc556b7ee4eb6183c13944c0e2052662567e2
jmielkeway <jmielkeway@gmail.com>
2021-02-21 01:41:46 +0100

    :computer: Initialize Free Lists in Chapter Six Code Zero (#81)
    Problem:
    ---
    - Need to manage memory
    - First must track all pages
    - Must also setup freelists
    
    Solution:
    ---
    - Create list struct and functions
    - Create system physical page directory to track all pages
    - Initialize page lists
    - Free baby boot and overwrite sections as well
    
    Issue: #80

---
+++ chapter6/code0/arch/arm64/lock.c
@@ -0,0 +1,32 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cake/lock.h"
+#include "arch/lock.h"
+
+extern void __irq_restore(unsigned long flags);
+extern unsigned long __irq_save();
+
+unsigned long spin_lock_irqsave(struct spinlock *lock)
+{
+    unsigned long flags = __irq_save();
+    __spin_lock(lock);
+    return flags;
+}
+
+void spin_unlock_irqrestore(struct spinlock *lock, unsigned long flags)
+{
+    __spin_unlock(lock);
+    __irq_restore(flags);
+}
