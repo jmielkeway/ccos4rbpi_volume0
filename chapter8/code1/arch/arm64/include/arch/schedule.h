69012bb84b2b395959555d83d2f28314b3e7d717
jmielkeway <jmielkeway@gmail.com>
2021-05-09 19:03:30 +0000

    :computer: Implement Work-Queues in Chapter Eight Code One
    Problem:
    ---
    - Sometimes it is profitable or necessary to defer work to a later time when it can be scheduled
    - In order to have this capability, work-queues need to be implemented
    - Needs demonstration
    
    Solution:
    ---
    - Add kernel thread to perform deferred work
    
    Issue: #208

---
+++ chapter8/code1/arch/arm64/include/arch/schedule.h
@@ -0,0 +1,30 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _ARCH_SCHEDULE_H
+#define _ARCH_SCHEDULE_H
+
+#include "cake/process.h"
+
+#define CONTEXT_SWITCH      __cpu_switch_to
+#define CURRENT             __current()
+#define SCHEDULE_CURRENT    __schedule_current_init
+#define FORK_PREEMPT_COUNT  (2)
+
+struct process *__cpu_switch_to(struct process *prev, struct process *next);
+struct process *__current();
+void __schedule_current_init();
+
+
+#endif
