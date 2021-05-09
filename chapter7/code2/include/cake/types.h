41336ec6ed6a02fd89f3d006fe04e287fb1884da
jmielkeway <jmielkeway@gmail.com>
2021-05-09 11:27:31 +0200

    :computer: Do Forking in Chapter Seven Code Two (#206)
    Problem:
    ---
    - In order to create processes, need to develop forking functionality
    
    Solution:
    ---
    - Add error checking for allocator
    - Move `schedule_current` to `schedule_init`
    - Copy a processes and initialize with `__ret_from_fork`
    - Allocate a new kernel stack
    
    Issue: #205

---
+++ chapter7/code2/include/cake/types.h
@@ -0,0 +1,39 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _CAKE_TYPES_H
+#define _CAKE_TYPES_H
+
+typedef __signed__ char __s8;
+typedef unsigned char __u8;
+
+typedef __signed__ short __s16;
+typedef unsigned short __u16;
+
+typedef __signed__ int __s32;
+typedef unsigned int __u32;
+
+__extension__ typedef __signed__ long long __s64;
+__extension__ typedef unsigned long long __u64;
+
+typedef __s8  s8;
+typedef __u8  u8;
+typedef __s16 s16;
+typedef __u16 u16;
+typedef __s32 s32;
+typedef __u32 u32;
+typedef __s64 s64;
+typedef __u64 u64;
+
+#endif
